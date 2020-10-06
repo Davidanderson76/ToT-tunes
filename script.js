529abf7fe553c3b2a01922d7753c143bf001c579
David Anderson <david.anderson1993@gmail.com>
2020-10-06 15:03:24 -0500

    updating the script file with working API function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var lyrics = "American idiot";
-    var queryUrl = "https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=" + lyrics + "&page_size=10&page=1&s_track_rating=desc&apikey=3810df36308c6384072e9fa2a9a3bde3";
+    var queryUrl = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=" + lyrics + "&page_size=10&page=1&s_track_rating=desc&apikey=3810df36308c6384072e9fa2a9a3bde3";
     
 
 
@@ -13,9 +13,3 @@ $(document).ready(function(){
 })
 
 
-// https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&apikey=3810df36308c6384072e9fa2a9a3bde3
-
-
-// http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=American+idiot&page_size=3&page=1&s_track_rating=desc&apikey=3810df36308c6384072e9fa2a9a3bde3
-
-// http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=on+tour+at+a+concert&page_size=10&page=1&s_track_rating=desc&apikey=3810df36308c6384072e9fa2a9a3bde3
